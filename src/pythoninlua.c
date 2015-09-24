fbc6067f174e02d3c8f436e8d703a2b22d771eff
Alex Sandro <geniofuturo@gmail.com>
2015-09-24 20:43:16 -0300

    Fixed bug in string conversion.

--- src/pythoninlua.c
+++ src/pythoninlua.c
@@ -143,6 +143,26 @@ static int py_convert_custom(lua_State *L, PyObject *pobj, int asindx) {
     return 1;
 }
 
+char *get_pyobject_as_string(lua_State *L, PyObject *o) {
+    char *s = PyString_AsString(o);
+    if (!s) {
+        PyErr_Print();
+        lua_error(L, "error converting python string");
+    }
+    return s;
+}
+
+char *get_pyobject_as_utf8string(lua_State *L, PyObject *o) {
+    if (PyUnicode_Check(o)) {  // need conversion
+        o = PyUnicode_AsUTF8String(o);
+        if (!o) {
+            PyErr_Print();
+            lua_error(L, "error converting unicode string");
+        }
+    }
+    return get_pyobject_as_string(L, o);
+}
+
 int py_convert(lua_State *L, PyObject *o) {
     int ret = 0;
     if (o == Py_None || o == Py_False) {
@@ -156,21 +176,13 @@ int py_convert(lua_State *L, PyObject *o) {
         Py_ssize_t len;
         char *s = PyUnicode_AsUTF8AndSize(o, &len);
 #else
-    } else if (PyString_Check(o)  || PyUnicode_Check(o)) {
+    } else if (PyString_Check(o)) {
+        lua_pushstring(L, get_pyobject_as_string(L, o));
+        ret = 1;
+    } else if (PyUnicode_Check(o)) {
+        char *s = get_pyobject_as_utf8string(L, o);
 #endif
-        Py_ssize_t len;
-        char *s = NULL;
-        PyString_AsStringAndSize(o, &s, &len);
-#ifndef PyUnicode_AsUTF8AndSize // Try convert to utf8
-        if (s == NULL && PyUnicode_Check(o)) {
-            o = PyUnicode_AsUTF8String(o);
-            PyString_AsStringAndSize(o, &s, &len);
-            if (s == NULL) {
-                lua_error(L, "error converting unicode string");
-            }
-        }
-#endif
-        lua_pushlstring(L, s, len);
+        lua_pushstring(L, s);
         ret = 1;
 #if PY_MAJOR_VERSION < 3
     } else if (PyInt_Check(o)) {
