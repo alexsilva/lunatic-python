ac3eb815159927f36462c74140a1b02b1f3dce86
Alex Sandro <geniofuturo@gmail.com>
2015-09-25 09:39:11 -0300

    Test args, kwargs.

---
+++ test2.lua
@@ -0,0 +1,52 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: alex
+-- Date: 24/09/2015
+-- Time: 18:53
+-- To change this template use File | Settings | File Templates.
+--
+
+local handle, msg = loadlib(getenv("LIBRARY_PATH"))
+if (not handle or handle == -1) then error(msg) end
+
+callfromlib(handle, 'luaopen_python')
+assert(python, "undefined python object")
+
+-- python home
+python.system_init("C:\\Python27");
+
+local builtins = python.builtins()
+local locals = python.locals()
+
+python.execute([[
+def TestFunc(*args, **kwargs):
+    _args = (1, 2, 3, 4, 5, ('a', 'c', 'd', 'e', {'a': 1, 'c': 3, 'b': 2}))
+    _kwargs = {
+        'host': '127.0.0.1',
+        'user': 'user',
+        'passwd': '12345&*',
+        'db': 'mysqdb'
+    }
+    assert args == _args, "[TestFunc] args no match!"
+
+    for k in _kwargs:
+        assert k in kwargs, "[TestFunc] key not found"
+        assert kwargs[k] == _kwargs[k], "[TestFunc] data no match!"
+    return True
+]])
+
+python.execute([[
+def TestFunc2(limit, *args, **kwargs):
+    assert range(1,limit) == list(args[0]), '[TestFunc2] args no match!'
+]])
+
+local index = 0
+while (index < 10) do
+    local args = python.args(1,2,3,4,5,{'a','c','d','e', {a=1,b=2,c=3}})
+    local kwargs = python.kwargs({host='127.0.0.1', user='user', passwd='12345&*', db='mysqdb'})
+    locals.TestFunc(args, kwargs)
+    index = index + 1
+end
+
+locals.TestFunc2(5, builtins.range(1,5))
+locals.TestFunc2(5, python.args(1,2,3,4))
