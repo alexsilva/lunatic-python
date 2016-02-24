8727d73940068d5afae02824eb91500c45b36dc4
Alex Sandro <geniofuturo@gmail.com>
2016-02-24 20:12:59 -0300

    Removing constant unused.

--- src/constants.h
+++ src/constants.h
@@ -10,13 +10,9 @@
 // Object wrap names
 #define PY_OBJECT "_python_object"
 #define PY_OBJECT_BASE "_python_object_base"
-#define PY_ARGS_WRAP "_python_args"
-#define PY_KWARGS_WRAP "_python_kwargs"
-#define PY_OBJECT_INDEX "_python_object_index"
 #define PY_UNICODE_ENCODING_ERRORHANDLER "_python_unicode_encoding_errorhandler"
 #define PY_UNICODE_ENCODING "_python_unicode_encoding"
 #define PY_OBJECT_BY_REFERENCE "_python_object_by_reference"
-#define PY_OBJECT_META "_python_object_meta"
 
 // globals Lua
 #define PY_ARGS_ARRAY_FUNC "pyargs_array"
