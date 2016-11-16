274e904ba4c630f660828a0bbb7f5b7fde493522
Alex Sandro <geniofuturo@gmail.com>
2016-11-16 13:08:06 -0200

    Fixing bug discovered in the allocation of objects in the stack (lua_next - win).

--- src/lapi.c
+++ src/lapi.c
@@ -1,12 +1,53 @@
 //
 // Created by alex on 25/09/2015.
 //
+// Functions api that unfortunately were not found in the cgilua.
 
 #include "lapi.h"
 #include <ldo.h>
+#include <lmem.h>
 
-void luaD_checkstack(lua_State *L, int n) { } // missing runtime
+#ifndef STACK_LIMIT
+#define STACK_LIMIT  INT_MAX  /* arbitrary limit */
+#endif
+#define STACK_UNIT    128
 
+/*
+** generic allocation routine.
+*/
+void *luaM_realloc(lua_State *L, void *block, unsigned long size) {
+    size_t s = (size_t) size;
+    if (s != size)
+        lua_error(L, "memory allocation error: block too big");
+    if (size == 0) {
+        free(block);  /* block may be NULL, that is OK for free */
+        return NULL;
+    }
+    block = realloc(block, s);
+    if (block == NULL)
+        lua_error(L, memEM);
+    return block;
+}
+
+// Check stack size
+void luaD_checkstack(lua_State *L, int n) {
+    struct Stack *S = &L->stack;
+    if (S->last - S->top <= n) {
+        StkId top = S->top - S->stack;
+        int stacksize = (S->last - S->stack) + STACK_UNIT + n;
+        luaM_reallocvector(L, S->stack, stacksize, TObject);
+        S->last = S->stack + (stacksize - 1);
+        S->top = S->stack + top;
+        if (stacksize >= STACK_LIMIT) {  /* stack overflow? */
+            if (lua_stackedfunction(L, 100) == LUA_NOOBJECT)  /* 100 funcs on stack? */
+                lua_error(L, "Lua2C - C2Lua overflow"); /* doesn't look like a rec. loop */
+            else
+                lua_error(L, "stack size overflow");
+        }
+    }
+}
+
+// Place a new object at the top of the stack.
 static void lapi_pushobject(lua_State *L, TObject *o) {
     *L->stack.top = *o;
     incr_top;
