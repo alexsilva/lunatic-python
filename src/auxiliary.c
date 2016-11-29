33b5d5a64dfb4067c1bd144bf109bb3617010a5c
Alex Sandro <geniofuturo@gmail.com>
2016-11-28 23:02:57 -0200

    Implemented reading numbers.

--- src/auxiliary.c
+++ src/auxiliary.c
@@ -134,6 +134,52 @@ static int read_file(lua_State *L, PyObject *o, int *lastreadchar) {
     return 1;
 }
 
+/*private*/
+static void raise_number_error(lua_State *L, char *number) {
+    char *format = "converting number \"%s\" failed!";
+    char buff[buffsize_calc(2, format, number)];
+    sprintf(buff, format, number);
+    lua_new_error(L, &buff[0]);
+}
+
+void remove_spaces(char* source) {
+    char* i = source;
+    char* j = source;
+    while(*j != 0) {
+        *i = *j++;
+        if(!isspace(*i))
+            i++;
+    }
+    *i = 0;
+}
+
+static int read_number(lua_State *L, PyObject *o, int *lastreadchar) {
+    int success = read_pattern(L, o, "%s*%d*%.?%d*", lastreadchar);
+    int length = luaL_getsize(L);
+    if (!success && length == 0) {
+        return 0;  /* read fails */
+    }
+    char *buffer = luaL_buffer(L);
+    char numbuff[length + 1];
+    strncpy(numbuff, buffer, (size_t) length);
+    numbuff[length] = '\0';
+    remove_spaces(numbuff);
+    if (strcmp(numbuff, ".") == 0 || strlen(numbuff) == 0) {
+        return 0; /* read fails */
+    }
+    PyObject *number = PyString_FromString(numbuff);
+    if (!number) raise_number_error(L, &numbuff[0]);
+    PyObject *result = PyFloat_FromString(number, NULL);
+    Py_DECREF(number);
+    if (result) {
+        if (py_convert(L, result) == CONVERTED)
+            Py_DECREF(result);
+    } else {
+        raise_number_error(L, numbuff);
+    }
+    return success;
+}
+
 /* Function that allows you to read a python file using the same Lua pattern syntax */
 void py_readfile(lua_State *L) {
     lua_Object luaObject = lua_getparam(L, 1);
@@ -156,14 +202,14 @@ void py_readfile(lua_State *L) {
         luaL_resetbuffer(L);
         switch (luaL_findstring(L, p, options)) {
             case 0:  /* number */
-                lua_error(L, "read number not implemented!");
+                if (!read_number(L, pyFile, &lastreadchar)) return;
                 continue;  /* number is already pushed; avoid the "pushstring" */
             case 1:  /* line */
                 success = read_pattern(L, pyFile, "[^\n]*{\n}", &lastreadchar);
                 break;
             case 2: case 3:  /* file */
-                success = read_file(L, pyFile, &lastreadchar); /* always success */
-                break;
+                read_file(L, pyFile, &lastreadchar);
+                continue; /* already pushed; avoid the "pushstring" */
             case 4:  /* word */
                 success = read_pattern(L, pyFile, "{%s*}%S+", &lastreadchar);
                 break;
