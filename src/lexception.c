5040c129f9f87247049feb5f117fcd59799fb15a
alex <geniofuturo@gmail.com>
2017-11-08 11:59:13 -0200

    Correction in error formatting.

--- src/lexception.c
+++ src/lexception.c
@@ -7,6 +7,7 @@
 
 void lua_error_fallback(lua_State *L, PyObject *exc, const char *msg) {
     const char *traceback = lua_traceback_message(L);
-    PyErr_Format(exc, "%s\n%s", msg ? msg : "unknown error.", traceback);
+    const char *format = traceback == NULL ? "%s" :  "%s\n%s";
+    PyErr_Format(exc, format, msg ? msg : "unknown error.", traceback);
     free((void *) traceback);
 }
