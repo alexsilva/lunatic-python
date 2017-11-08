3a4e4fb593af8dd3ab5970afea23c41649971ee2
alex <geniofuturo@gmail.com>
2017-11-08 11:32:38 -0200

    Freeing up dynamic memory.

--- src/lexception.c
+++ src/lexception.c
@@ -6,6 +6,7 @@
 #endif
 
 void lua_error_fallback(lua_State *L, PyObject *exc, const char *msg) {
-    PyErr_Format(exc, "%s\n%s", msg ? msg : "unknown error.",
-                 lua_traceback_message(L));
+    const char *traceback = lua_traceback_message(L);
+    PyErr_Format(exc, "%s\n%s", msg ? msg : "unknown error.", traceback);
+    free((void *) traceback);
 }
